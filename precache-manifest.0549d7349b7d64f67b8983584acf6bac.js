a909c596c2dc2441e5c24ca0f4835f8ef23b75c1
bBanker <fireninja171@gmail.com>
2020-06-28 15:03:51 -0400

    Updates

---
+++ precache-manifest.0549d7349b7d64f67b8983584acf6bac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0720884804d6807c25a9d798debc6a69",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "63e44b4d6b71ecff423c",
+    "url": "/react-resume/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c21c8425b86653809c3a",
+    "url": "/react-resume/static/css/main.c6a58a8e.chunk.css"
+  },
+  {
+    "revision": "63e44b4d6b71ecff423c",
+    "url": "/react-resume/static/js/2.5263b5b3.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/react-resume/static/js/2.5263b5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21c8425b86653809c3a",
+    "url": "/react-resume/static/js/main.34eb3e8a.chunk.js"
+  },
+  {
+    "revision": "7f53f61ad472ba31718b",
+    "url": "/react-resume/static/js/runtime-main.b82ab2ef.js"
+  }
+]);

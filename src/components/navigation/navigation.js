05bd1ef22ce1cbc9e5bcf6f1c80c84d9caffc98b
bBanker <fireninja171@gmail.com>
2020-07-11 12:27:49 -0400

    added icons

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import NavigationItem from './navigationItem';
+import {AiOutlineMail,AiFillGithub,AiOutlineDownload} from 'react-icons/ai';
+import './navigation.css';
 
 const Navigation = props => (
     <nav id="navigation" className="navbar navbar-expand-lg navbar-dark bg-dark">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto" id="static-links">
+            <a className="dropdown-item" href={window.location + "documents/Resume.pdf" } download><AiOutlineDownload/></a>
+            <a className="dropdown-item" href="http://www.github.com/bBankert"><AiFillGithub/></a>
+            <a className="dropdown-item" href="mailto:bankert.brandon@gmail.com"><AiOutlineMail/></a>
+        </ul>
+      <div className="collapse navbar-collapse mr-auto" id="navbarNav">
         <ul className="navbar-nav">
           {props.data.map((link,idx) => 
               <NavigationItem key={'link-'+idx} link={link}/>
           )}
           {/* Static links */}
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Contact
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href={window.location + "documents/Resume.pdf" } download>Download Resume</a>
-              <a className="dropdown-item" href="http://www.github.com/bBankert">GitHub</a>
-              <a className="dropdown-item" href="mailto:bankert.brandon@gmail.com">Contact Me</a>
-            </div>
-          </li>
         </ul>
       </div>
+      <ul className="navbar-nav ml-auto" id="static-links-desktop">
+            <a className="dropdown-item" href={window.location + "documents/Resume.pdf" } download><AiOutlineDownload/></a>
+            <a className="dropdown-item" href="http://www.github.com/bBankert"><AiFillGithub/></a>
+            <a className="dropdown-item" href="mailto:bankert.brandon@gmail.com"><AiOutlineMail/></a>
+        </ul>
     </nav>
 
 );

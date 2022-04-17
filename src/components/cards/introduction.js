32c18f5d71a242c8b72fe3cfbc42762f54874977
Brandon Bankert <fireninja171@gmail.com>
2022-04-17 10:25:28 -0400

    Upgrade to 18 and cleanup

--- src/components/cards/introduction.js
+++ src/components/cards/introduction.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './introduction.css';
 
 const Introduction = props => {
--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,33 +1,12 @@
-import React from 'react';
-import $ from 'jquery';
 import './loading.css';
 
-class Loading extends React.Component {
-
-    componentDidMount(){
-        $('#loadingModal').modal('show');
-    }
-
-    componentWillUnmount(){
-        $('#loadingModal').modal('hide');
-    }
-
-
-
-    render(){
-        return(<div id="loading" className="col-md-6 offset-md-3 col-sm-12">
-            <div className="modal" id="loadingModal" tabIndex="-1" role="dialog">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-body">
-                            <div className="loader"></div>
-                            <p>Loading, Please Wait...</p>
-                        </div>
-                    </div>
+const Loading = () => {
+    return(<div className="loading-modal">
+                <div className="modal-content">
+                    <div className="loader"></div>
+                    <p>Loading, Please Wait...</p>
                 </div>
-            </div>
-        </div>);
-    }
+            </div>);
 }
 
 export default Loading;
